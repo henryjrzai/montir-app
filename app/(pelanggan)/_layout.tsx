20fbe62ead2f6c5a9e1ef2c639aa4a8774047e6a
Henry Jr Zai <hjuniorzai@gmail.com>
2025-11-18 01:42:14 +0700

    [ADD] : setup menu untuk pelanggan

--- app/(pelanggan)/_layout.tsx
+++ app/(pelanggan)/_layout.tsx
@@ -4,6 +4,8 @@
 
 import { Tabs } from "expo-router";
 import { Colors } from "../../src/constants/colors";
+import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function PelangganLayout() {
   return (
@@ -17,12 +19,39 @@ export default function PelangganLayout() {
         name="index"
         options={{
           title: "Beranda",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search-bengkel"
+        options={{
+          title: "Cari Bengkel",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="search" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profil",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user-circle" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bengkel-detail"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="order-detail"
+        options={{
+          href: null,
         }}
       />
     </Tabs>
